56fc65ad976c0e3c57a631a925cbb6a8896b0013
callac2 <callac2@nycstudents.net>
2026-04-16 09:25:53 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,36 +11,38 @@ def pokemon(json):
     for i in pokemon[name]:
         name.append(pokemon)
         print(name) """
-for index, pokemon in enumerate(info):
+for index, pokemon in (info):
     print(index, ":", pokemon["name"])
 
 # Add a language choice feature and print the pokemons name based on the user input
-""" 
+
 language_choice = input("enter your desired language: (1)English, (2)French, (3)Japanese, or (4)Chinese")
 if language_choice == "1":
-    for index, pokemon in enumerate(info):
+    for index, pokemon in (info):
             print(index, ":", pokemon["name"]['english'])
     
 elif language_choice == "2":
-    for index, pokemon in enumerate(info):
+    for index, pokemon in (info):
         print(index, ":", pokemon["name"]['french'])
 
 elif language_choice == "3":
-    for index, pokemon in enumerate(info):
+    for index, pokemon in (info):
         print(index, ":", pokemon["name"]['japanese'])
 
 else:
         if language_choice == "4":
-            for index, pokemon in enumerate(info):
-             print(index, ":", pokemon["name"]['chinese']) """
+            for index, pokemon in (info):
+             print(index, ":", pokemon["name"]['chinese'])
         
 # Develop a function that creates a new list of pokemon based on the type the user searched for. If no pokemon was found of that type inform the user
-search_type = input("enter your pokemon's type.")
 
+""" 
 for index, pokemon in enumerate(info):
    print(pokemon["type"])
-   """   if search_type == 
- """
+
+
+search_type = input("enter your pokemon's type.")
+if search_type ==  """
 #Develop a function to find all pokemon matching the name the user searched for. Ex. if "Char" return Charmander, Charmeleon and Charizard. Make the user aware if no pokemon was found. 
 
 #For Leo/, help me come up with a clever final question, considering maybe showing all moves a pokemon has avaiable based on type
