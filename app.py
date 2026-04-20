6bfdb945b76d88d2ab06cdf63d062f01eae85045
callac2 <callac2@nycstudents.net>
2026-04-20 09:26:15 -0400

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def pokemon(json):
 
 # Add a language choice feature and print the pokemons name based on the user input
 
-language_choice = input("enter your desired language: (1)English, (2)French, (3)Japanese, or (4)Chinese")
+""" language_choice = input("enter your desired language: (1)English, (2)French, (3)Japanese, or (4)Chinese")
 for pokemon in (info):
     if language_choice == "1":      
      print(pokemon["name"]['english'])
@@ -31,32 +31,30 @@ for pokemon in (info):
 
 else:
         if language_choice == "4":
-             print(pokemon["name"]['chinese'])
+             print(pokemon["name"]['chinese']) """
         
 # Develop a function that creates a new list of pokemon based on the type the user searched for. If no pokemon was found of that type inform the user
 
 
-""" for index, pokemon in (info):
-   print(pokemon["type"]) """
+for pokemon in (info):
+   print(pokemon["type"])
 
 
 search_type = input("enter your pokemon's type.")
-""" if search_type """
-search_1 = True
-for pokemon in info:
-        if search_type in info:
-            print (pokemon['type'])
-characters = {}
 
-while True:
-            pokemon['type'] == search_type 
+type_list = {}
+
+for pokemon in info:
+        if pokemon[search_type]["type"] in info:
+            type_list.append(search_type)
+
             
 
-     
-        print(pokemon['type'])
 
 
 #Develop a function to find all pokemon matching the name the user searched for. Ex. if "Char" return Charmander, Charmeleon and Charizard. Make the user aware if no pokemon was found. 
-
+match = (input("search word:"))
+if match in info:
+     print [match]["name"]
 #For Leo/, help me come up with a clever final question, considering maybe showing all moves a pokemon has avaiable based on type
 
